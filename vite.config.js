d7e14d9a28309b15b720f13036fa852a1c3966e4
IanCo0k <ianscook19@gmail.com>
2023-08-19 15:37:56 -0400

    removing compression

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import CompressionPlugin from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
-    react(),
-    CompressionPlugin({
-      algorithm: 'gzip',  // Use gzip compression
-      ext: /\.jsx$/       // Apply compression to .jsx files
-    })
+    react()
   ]
 });
-
