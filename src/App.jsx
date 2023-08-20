bb42cf30f642e3f56bbae901181b1b8881e3321a
IanCo0k <ianscook19@gmail.com>
2023-08-20 13:27:47 -0400

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Dropdown from './components/Dropdown';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
+import { getAnalytics, logEvent } from "firebase/analytics";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +24,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 import b1gLogo from './b1g.png';
 import pacLogo from './pac.png';
