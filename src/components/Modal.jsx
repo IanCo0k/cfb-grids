55d685dd530770914f3f5e544681bd7a8e9d27ef
IanCo0k <ianscook19@gmail.com>
2023-09-06 16:25:26 -0400

    hopefully this modal is better

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal() {
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-          <div className="bg-white p-6 rounded shadow-md z-10">
+          <div className="bg-white p-6 rounded shadow-md z-10" style={{ width: '80%', maxHeight: '500px', overflowY: 'auto' }}>
             <h1 className='text-center text-2xl font-semibold mb-4'>
               Support CFB Grids {' '}
               <a
@@ -24,6 +24,18 @@ function Modal() {
               HERE
               </a>
             </h1>
+            <div style={{ borderRadius: '12px', margin: '1em' }}>
+              <iframe
+                src="https://open.spotify.com/embed/show/3PxjY8VH1h4Q4Cb2shJqRQ?utm_source=generator"
+                width="100%"
+                height="352"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+                title="Spotify Embed"
+              ></iframe>
+            </div>
             <p className="text-center text-2xl mb-4">TODAY'S PLAYERS ARE BACK TO BETWEEN 1956-2022/23 SEASON</p>
             <p className="text-center text-2xl mb-4">Should also accept transfer players, granted they met the career total with the school.</p>
             <p className='text-center mb-4'>
