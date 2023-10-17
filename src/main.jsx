5b700152e6cd866d49d0d619337434d1e17b00f4
IanCo0k <ianscook19@gmail.com>
2023-10-17 12:03:09 -0400

    reverting back. ugg

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import CFB from './CFB.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CFB />
   </React.StrictMode>,)
