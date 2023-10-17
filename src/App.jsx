de7a2992d84cadc1baaa66b52704ba46dbf042e1
IanCo0k <ianscook19@gmail.com>
2023-10-17 11:58:15 -0400

    let's try this again

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
-import CBB from './CBB';
+import React, { useState, Suspense } from 'react';
 import CFB from './CFB';
+import CBB from './CBB';
 
 export default function App() {
   const [selectedComponent, setSelectedComponent] = useState('CFB');
 
-  const renderComponent = () => {
-    if (selectedComponent === 'CFB') {
-      return <CFB />;
-    } else if (selectedComponent === 'CBB') {
-      return <CBB />;
-    }
-  };
-
   return (
-    <div className='bg-gray-800'>
+    <div className="bg-gray-800">
       <nav className="bg-blue-500 p-4 flex justify-center">
         <button
           className={`text-white font-bold py-2 px-4 rounded mx-2 ${
@@ -34,7 +26,9 @@ export default function App() {
         </button>
       </nav>
       <div className="container mx-auto p-4">
-        {renderComponent()}
+        <Suspense fallback={<div>Loading...</div>}>
+          {selectedComponent === 'CFB' ? <CFB /> : <CBB />}
+        </Suspense>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CFB from './CFB.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CFB />
+    <App />
   </React.StrictMode>,)
