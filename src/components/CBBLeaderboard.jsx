762cf9e35c15e6dfe6e3e7b7a0e08ae707d06de3
IanCo0k <ianscook19@gmail.com>
2023-10-20 11:05:40 -0400

    NEW GRID DROP OCT 20

--- src/components/CBBLeaderboard.jsx
+++ src/components/CBBLeaderboard.jsx
@@ -13,7 +13,7 @@ function CBBLeaderboard({ imgSrc1, imgSrc2, imgSrc3 }) {
   useEffect(() => {
     const fetchTopScores = async () => {
       const db = getFirestore();
-      const leaderboardRef = doc(db, 'dailyLeaderboard', 'cbb-oct19leaders');
+      const leaderboardRef = doc(db, 'dailyLeaderboard', 'cbb-oct20leaders');
       
       try {
         const docSnapshot = await getDoc(leaderboardRef);
